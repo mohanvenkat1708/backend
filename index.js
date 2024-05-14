dcd7334ef96ef0954eb951e050d21bf8f35d0314
mohanvenkat1708 <mohanvenkat1708@gmail.com>
2024-05-14 08:58:05 +0530

    added the database credentials in .env file

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ app.use(cors({
 app.use(cookieParser());
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'sago'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 // Signup route
